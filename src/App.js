bb3c1c9995687cdca722f20d1bdf857139ee9333
JoSooha <dlrhdcjs@naver.com>
2022-07-29 23:56:26 +0900

    0729 my-react-app todo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import Button from "./Button"; //button.js에서 버튼 컴포 가져옴
-import styles from "./App.module.css";
+import {useState, useEffect} from "react";
+
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+
   return (
     <div>
-      <h1 className={styles.title}>welcome back!</h1>
-      <Button text={"Continue"} />
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr /> 
+      <ul>
+        {toDos.map((item, index) => (  //todos 어레이에서 각 값을 item으로 꺼내와 list에 담기(변환) 이후 각 key값을 index로 주어 콘솔 오류를 없앰
+          <li key={index}> {item} </li>
+        ))}
+      </ul>
     </div>
   );
 }
