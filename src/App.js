284a0da13f92f6bbe5bf576f3e124454bfa01fc4
JoSooha <dlrhdcjs@naver.com>
2022-08-04 18:02:58 +0900

    0804 React 영화 나머지 중

--- src/App.js
+++ src/App.js
@@ -1,40 +1,47 @@
-import {useState, useEffect} from "react";
+import { useEffect, useState } from "react"
 
-
-function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
+function App () {
+    const [loading, setLoading] = useState(true)
+    const [coins, setCoins] = useState([])
+    const [usd, setUsd] = useState()
+    const onchange = (event) => setUsd(event.target.value); //인풋
+    const onSubmit = (event) => { //인풋 제출
+        event.preventDefault();
+        if(usd==="")
+            {return;}
+        setLoading(false)
+        setUsd("");
     }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
 
-  return (
+    useEffect(() => { //api 한번만 불러오면됨
+        fetch("https://api.coinpaprika.com/v1/tickers") //원격 api 호출하는 방법
+            .then((response) => response.json()) //리턴 받은 것 json 변환
+            .then((json) => { 
+                setCoins(json); //그 json을 coin으로 변환
+            });
+    }, [usd])
+
+    return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr /> 
-      <ul>
-        {toDos.map((item, index) => (  //todos 어레이에서 각 값을 item으로 꺼내와 list에 담기(변환) 이후 각 key값을 index로 주어 콘솔 오류를 없앰
-          <li key={index}> {item} </li>
-        ))}
-      </ul>
-    </div>
-  );
+        <h1> The coins!</h1> 
+
+        <form onSubmit={onSubmit}> {/*변환해보려는 usd값 입력받음*/}
+            <input onChange={onchange} type="number" value={usd} placeholder="type usd"></input>
+        </form>
+
+        {loading ? (
+            <strong> Loading...</strong>
+        ) : ( //변환된 값 출력
+             <select>
+                {coins.map((coin, index) => ( //coins 어레이 각 리스트 하나씩 뽑아옴
+                    <option key={index}>
+                        {coin.name} ({coin.symbol}): ${usd/(coin.quotes.USD.price)} USD
+                    </option>
+                ))}
+            </select>
+
+        )}
+    </div>)
 }
 
 export default App;
- 
